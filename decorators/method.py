c04af0aae975147afbb54b6d1c09c464a5587b53
Rafael Duran Castaneda <rafadurancastaneda@gmail.com>
2011-09-24 16:07:14 +0200

    Added memoized to pyrdc.decorators.method

--- decorators/method.py
+++ decorators/method.py
@@ -11,12 +11,14 @@ This module contains method decorators.
     
 .. moduleauthor::"Rafael Durán Castañeda <rafadurancastaneda@gmail.com>"
 """
+import functools
 
 def optional_arguments_decorator(real_decorator):
     """
-    :py:func:`optional_arguments_decorator` is a decorates others decorators, so 
-    they can be called with or without arguments. This receipt has been taken from 
-    chapter 2 of Martyn Alchin's "Pro Django" book.
+    :py:func:`optional_arguments_decorator` is a decorator factory, so a 
+    function is converted in a new decorator that can be used with or without 
+    arguments. This receipt has been taken from  chapter 2 of 
+    Martyn Alchin's "Pro Django" book.
     
     .. note::
         Decorators using this receipt must used extra arguments as keywords 
@@ -57,3 +59,46 @@ def optional_arguments_decorator(real_decorator):
         # The decorator was called without arguments
         return decorated(func)
     return decorator
+
+
+class memoized(object):
+    """
+    :py:class:`memoized` decorator caches a function's return value each time 
+    it is called. If called later with the same arguments, the cached value is 
+    returned, and not re-evaluated. This decorator recipe has been taking from 
+    `Python decorators libray <http://wiki.python.org/moin/PythonDecoratorLibrary>`_
+    
+    Usage::
+    
+        @memoized
+        def fibonacci(n):
+           "Return the nth fibonacci number."
+           if n in (0, 1):
+              return n
+           return fibonacci(n-1) + fibonacci(n-2)
+
+    Then::
+    
+        >>> print fibonacci(12)
+        144
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        try:
+            return self.cache[args]
+        except KeyError:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+        except TypeError:
+            # uncachable -- for instance, passing a list as an argument.
+            # Better to not cache than to blow up entirely.
+            return self.func(*args)
+        def __repr__(self):
+            """Return the function's docstring."""
+            return self.func.__doc__
+        def __get__(self, obj, objtype):
+            """Support instance methods."""
+            return functools.partial(self.__call__, obj)
